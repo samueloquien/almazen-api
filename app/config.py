046d24199a287943d0d13ef78993508aad3a29c0
Samuel Peláez <samuel.pelaez@neuroelectrics.com>
2020-05-11 00:42:35 +0200

    Move config.py to application entrypoint level

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
-
-class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'mysql://AlmazenAPI:S1403p++@localhost/almazen-db'
-
