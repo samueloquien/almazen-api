48d96672d906f531fc7bc0319a6f7350f6436582
Samuel Peláez <samuel.pelaez@neuroelectrics.com>
2020-05-10 23:11:53 +0200

    Rename server folder into app

--- app/models/languages.py
+++ app/models/languages.py
@@ -1,4 +1,4 @@
-from server import db
+from app import db
 
 class Language(db.Model):
     lang_id = db.Column(db.Integer, primary_key=True)
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from server import create_app
+from app import create_app
 
 try:
 	
