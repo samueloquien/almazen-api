f87be71de4bfccaafa42beda2dc56bf83c7f8859
Samuel Peláez <samuel.pelaez@neuroelectrics.com>
2020-05-10 23:09:16 +0200

    Add model for table languages
    Doesn't work yet

---
+++ server/models/languages.py
@@ -0,0 +1,8 @@
+from server import db
+
+class Language(db.Model):
+    lang_id = db.Column(db.Integer, primary_key=True)
+    lang = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Language %r>' % self.lang
