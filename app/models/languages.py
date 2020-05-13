716907a4b11faf648639715a3025950b9008e6e2
Samuel Peláez <samuel.pelaez@neuroelectrics.com>
2020-05-14 00:32:08 +0200

    Make languages.language_lang 5 chars length
    Fixes #9

--- app/models/languages.py
+++ app/models/languages.py
@@ -2,7 +2,7 @@ from app import db
 
 class Languages(db.Model):
     language_id = db.Column(db.Integer, primary_key=True)
-    language_lang = db.Column(db.String(80), unique=True, nullable=False)
+    language_lang = db.Column(db.String(5), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Language %r>' % self.lang
